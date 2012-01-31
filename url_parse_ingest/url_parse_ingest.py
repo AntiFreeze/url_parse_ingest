507bf5891a593dcf3d1e42fbba4145d537fe4d72
Seth Blank <s@yourtrove.com>
2012-01-31 15:19:25 -0800

    get headers

--- url_parse_ingest/url_parse_ingest.py
+++ url_parse_ingest/url_parse_ingest.py
@@ -1,9 +1,10 @@
+import sys
 import re, json
-import urllib, urllib2
+import urllib2
 
 class jsonDocument():
 
-    def __init__(self, url):
+    def __init__(self, url, fetch=True):
         self._url = url
 
         self._filetype = None
@@ -11,8 +12,23 @@ class jsonDocument():
         self._body = None
         self._parsed = None
         self._ingested = None
+        self._response = None
 
         self._DEBUG = False
 
-    def getHeader(self):
-        pass
+        if fetch:
+            pass
+
+    def _debugPrint(self, message):
+        if self._DEBUG:
+            print >> sys.stderr, message
+
+    def setDebug(self, debug=True):
+        self._DEBUG = debug
+        message = "ENABLED" if debug else "DISABLED"
+        print >> sys.stderr, "Debugging:", message
+
+    def _getHeader(self):
+        if not self._response:
+            self._response = urllib2.urlopen(self._url)
+        return self._response.info()
