1ae5523ee70115a545db57d5c28a18ac79500d08
Seth Blank <s@yourtrove.com>
2012-01-31 16:44:32 -0800

    mods

--- url_parse_ingest/url_parse_ingest.py
+++ url_parse_ingest/url_parse_ingest.py
@@ -4,7 +4,22 @@ import urllib2
 
 class registeredHandlers():
     def __init__(self):
-        self._handlers = {}
+        self._handlers = { 'unknown':self._unknown_handler }
+
+    def __str__(self):
+        message = "registeredHandlers(%d):" % len(self._handlers)
+        for handler in self._handlers:
+            message = message + handler
+        return message
+
+    def __repr__(self):
+        message = "<url_parse_ingest.registeredHandlers(%d):" % len(self._handlers)
+        for handler in self._handlers:
+            message = message + " " + handler
+        return message + ">"
+
+    def _unknown_handler(self, data):
+        return {'title':'unknown', 'body':'unknown', 'content-length':'unknown'}
 
     def isHandler(self, type):
         return True if type in self._hanlders else False
@@ -22,7 +37,7 @@ class registeredHandlers():
 
 class jsonDocument():
 
-    def __init__(self, url):
+    def __init__(self, url, handlers):
         self._url = url
 
         # The bits we care about
@@ -30,6 +45,7 @@ class jsonDocument():
         self._title = None
         self._body = None
         self._ingested = None
+        self._handlers = handlers
 
         # HTTP information
         self._content_length = None
@@ -48,29 +64,37 @@ class jsonDocument():
             self._response.close()
 
     def __str__(self):
-        return "jsonDocument (ingested=%s) %s: type %s, title %s, size: %d" % (self._ingested, self._url, self._filetype, self._title, self._content_length)
+        return "jsonDocument (ingested=%s) %s: type %s, title %s, size: %s" % (self._ingested, self._url, self._filetype, self._title, self._content_length)
 
     def __repr__(self):
-        return "<url_parse_ingest.jsonDocument ingested=%s, url=%s: type %s, title %s, size: %d>" % (self._ingested, self._url, self._filetype, self._title, self._content_length)
+        return "<url_parse_ingest.jsonDocument ingested=%s, url=%s: type %s, title %s, size: %s>" % (self._ingested, self._url, self._filetype, self._title, self._content_length)
 
     def _debugPrint(self, message):
         if self._DEBUG:
             print >> sys.stderr, message
 
-    def _getHeaders(self):
-        headers = []
+    def _openURL(self):
         if not self._response:
             try:
                 self._response = urllib2.urlopen(self._url)
-                headers = self._response.info()
-                content_type = headers['content-type'].split(';')
-                self._content_length = headers['content-length']
-                self._filetype = content_type[0]
-                self._charset = content_type[1]
-                self._debugPrint("_getHeader(%s): filetype: %s, charset: %s, length: %s" % (self._url, self._filetype, self._charset, self._content_length))
+                if self._response.status != 200:
+                    # do the right thing
+                    # account for redirects and try not to loop
+                    pass
             except:
                 self._failed = True
-                self._debugPrint("_getHeader() failed with unexpected error %s" % sys.exc_info()[0])
+                self._debugPrint("_openURL() failed with unexpected error %s" % sys.exc_info()[0])
+
+    def _getHeaders(self):
+        headers = []
+        self._openURL()
+        if not self._response:
+            headers = self._response.info()
+            content_type = headers.get('content-type','unknown;unknown').split(';')
+            self._content_length = headers.get('content-length',0)
+            self._filetype = content_type[0]
+            self._charset = content_type[1]
+            self._debugPrint("_getHeader(%s): filetype: %s, charset: %s, length: %s" % (self._url, self._filetype, self._charset, self._content_length))
 
         return headers
 
@@ -84,11 +108,6 @@ class jsonDocument():
         if not self._response:
             return {}
 
-        if self._filetype == "text/html":
-            pass
-        elif self._filetype == "applications/pdf":
-            pass
-        elif self._filetype == "applications/docx":
-            pass
+        response = self._handlers.callHandler(self._filetype)
 
-        return {}
+        return response
