9584c950bb7574ea131eda0de8facd1eda5d09ae
Seth Blank <s@yourtrove.com>
2012-02-02 11:50:11 -0800

    simplified

--- url_parse_ingest/settings.py
+++ url_parse_ingest/settings.py
@@ -11,7 +11,6 @@ DEBUG = False
 MASTER_HANDLER = None
 
 TITLE_REGEX = re.compile("<title>(.[^<]*)</title>")
-SCRIPT_REGEX = re.compile("<script.[^>]*>(.[^<]*)</script>")
 
 try:
     from local_settings import *    
